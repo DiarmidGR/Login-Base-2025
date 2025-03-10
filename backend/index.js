25104190012d2fb23c6f026fff01ef5d0d575281
diarmid <diarmid@hotmail.ca>
2025-03-10 17:01:32 -0600

    connected backend to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
+const {mongoose} = require('mongoose');
+
+// database connection
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log('Database Connected'))
+.catch((err) => console.log('Database Not Connected', err))
 
 const app = express();
 
