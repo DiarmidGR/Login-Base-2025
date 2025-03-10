adbaa9ee2642dcdcd20cd73d3c764e662e6b6ea2
diarmid <diarmid@hotmail.ca>
2025-03-10 14:40:19 -0600

    created Login form

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,19 @@
 
 export default function Login() {
+    
+    const loginUser = (e:any) => {
+        e.preventDefault()
+    }
+
   return (
-    <div>Login</div>
+    <div>
+        <form onSubmit={loginUser}>
+            <label>Email</label>
+            <input type='email' placeholder='enter email...'/>
+            <label>Password</label>
+            <input type='password' placeholder='enter password...'/>
+            <button type='submit'>Login</button>
+        </form>
+    </div>
   )
 }
