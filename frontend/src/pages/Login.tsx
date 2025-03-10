846540a098249dff1b19885a20cd748433c60007
diarmid <diarmid@hotmail.ca>
2025-03-10 14:49:16 -0600

    finished base of login and register page before backend implementation

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react"
 
 export default function Login() {
     
+    const [data, setData] = useState({
+        email: '',
+        password: '',
+    })
+
     const loginUser = (e:any) => {
         e.preventDefault()
     }
@@ -9,9 +15,9 @@ export default function Login() {
     <div>
         <form onSubmit={loginUser}>
             <label>Email</label>
-            <input type='email' placeholder='enter email...'/>
+            <input type='email' placeholder='enter email...' value={data.email} onChange={(e) => setData({...data, email: e.target.value})}/>
             <label>Password</label>
-            <input type='password' placeholder='enter password...'/>
+            <input type='password' placeholder='enter password...' value={data.password} onChange={(e) => setData({...data, password: e.target.value})}/>
             <button type='submit'>Login</button>
         </form>
     </div>
